9b280c8d6a477ced404854ab3ca168f549718c6a
anth <zyle1104@gmail.com>
2024-12-23 17:33:57 +0900

    ⚰️ Remove: 미사용 테스트패키지 및 코드 삭제

--- src/main/java/org/example/zoomingle/infrastructure/core/test/TestJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.zoomingle.infrastructure.core.test;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestJpaRepository extends JpaRepository<TestEntity, Long> {
-}
